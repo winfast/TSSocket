47c9fb85c86d812e2651b4629998acecaa9b0bf3
QinChuancheng <qincc@galanz.com>
2019-07-18 15:56:25 +0800

    更新版本

--- TSSocket/Classes/TSBaseSocket.m
+++ TSSocket/Classes/TSBaseSocket.m
@@ -10,17 +10,33 @@
 
 @implementation TSBaseSocket
 
+/**
+ 连接tcp服务器 子类必须实现
+ 
+ @param ipAddress 服务器IP地址
+ @param port 服务器端口
+ @return 连接状态
+ */
 - (BOOL)connectTCPServer:(NSString *)ipAddress port:(NSInteger)port
 {
     return YES;
 }
 
-//
+/**
+ 断开TCP连接  子类必须实现
+ 
+ @return 是否断开 YES 断开
+ */
 - (BOOL)disConnect
 {
     return YES;
 }
 
+/**
+ 发送TCP数据流
+ 
+ @param postData 发送的内容
+ */
 - (void)postData:(NSDictionary *)postData
 {
     
